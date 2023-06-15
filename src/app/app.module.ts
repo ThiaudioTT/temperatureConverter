1a9ca6a92c27d66d1ff5dfe776fdbe46adee7226
ThiaudioTT <thiago.sky25@gmail.com>
2023-06-15 09:15:48 -0300

    feat: creates temperature converter component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TemperatureConverterComponent } from './components/temperature-converter/temperature-converter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemperatureConverterComponent
   ],
   imports: [
     BrowserModule,
